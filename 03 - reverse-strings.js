99cfb0058f843c3708c9116446c6225bf94cd85c
Elyse Galizio <elyse.galizio@gmail.com>
2023-01-09 19:08:43 +0100

    01, 02, 03 exercises complete

--- 03 - reverse-strings.js
+++ 03 - reverse-strings.js
@@ -4,11 +4,14 @@
 // 1. 'world'  =>  'dlrow'
 // 2. 'word'   =>  'drow'
 
+
 function reverseString(string) {
     // Write your code here
-}
+     return string.split('').reverse().join('');
+    }
+
 
 
 // DO NOT TOUCH TESTS BELOW
-console.log(booleanToWord('world') === 'dlrow' ? '✅ 01 - Test Passed' : '❌ 01 - Test Failed');
-console.log(evenOrOdd('word') === 'drow' ? '✅ 02 - Test Passed' : '❌ 02 - Test Failed');
+console.log(reverseString('world') === 'dlrow' ? '✅ 01 - Test Passed' : '❌ 01 - Test Failed');
+console.log(reverseString('word') === 'drow' ? '✅ 02 - Test Passed' : '❌ 02 - Test Failed');
